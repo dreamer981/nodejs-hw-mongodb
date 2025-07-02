52301fb4a4bf6cfe826a2e0f42eb31d834990ddf
dreamer981 <sdreamer590@gmail.com>
2025-07-02 22:50:35 +0300

    edit

--- src/server.js
+++ src/server.js
@@ -11,8 +11,6 @@ export const setupServer = () => {
 
   app.use(cors());
   app.use(pino());
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-
 
   app.use(express.json()); // JSON body parse için mutlaka ekle
 
